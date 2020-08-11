92ea169b17e55e62f3c4eb52e7a92a6baad0c892
Julien Blondeau <julienblondeau@live.fr>
2020-08-11 12:04:08 +0200

    Updates

---
+++ precache-manifest.fa0211c4eeff46d90a84dda49f391f7f.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc04d2e558821fddfa20f6921243c2cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b2d29875b3628f64598",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "698e72bca86b4fcda86e",
+    "url": "/static/css/main.d43fb766.chunk.css"
+  },
+  {
+    "revision": "9b2d29875b3628f64598",
+    "url": "/static/js/2.74496636.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.74496636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "698e72bca86b4fcda86e",
+    "url": "/static/js/main.bd088830.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "a6c35682ceae7d7d177fcf370541014c",
+    "url": "/static/media/background.a6c35682.png"
+  },
+  {
+    "revision": "8e3ea7328e6b3a13c78e7f165b5ca104",
+    "url": "/static/media/dart-game-stat.8e3ea732.PNG"
+  },
+  {
+    "revision": "49dd600495b325428d78fff5658fb2ee",
+    "url": "/static/media/dart-game.49dd6004.PNG"
+  },
+  {
+    "revision": "d5a06ecf322f68943abfa01953dedc39",
+    "url": "/static/media/dart-graph.d5a06ecf.PNG"
+  },
+  {
+    "revision": "f90a84c93c040e433b3f856c37da4ffe",
+    "url": "/static/media/dart-home.f90a84c9.PNG"
+  },
+  {
+    "revision": "4ea79c2a618943637189e839279a2495",
+    "url": "/static/media/dart-stats.4ea79c2a.PNG"
+  },
+  {
+    "revision": "4405bf3001ceed0ed859aedd6f12e25e",
+    "url": "/static/media/dart-win-loss.4405bf30.PNG"
+  },
+  {
+    "revision": "23fd1a25a846f754c9aa6f2212669f8d",
+    "url": "/static/media/learn-cards.23fd1a25.PNG"
+  },
+  {
+    "revision": "1efa34f3903089f4ccc18e043049f6ec",
+    "url": "/static/media/learn-create-course.1efa34f3.PNG"
+  },
+  {
+    "revision": "88d0fd087e4307b994b10b7db97bae8e",
+    "url": "/static/media/learn-create-quiz.88d0fd08.PNG"
+  },
+  {
+    "revision": "fc59894e38f9b6bd312aac1e5494ed00",
+    "url": "/static/media/learn-home.fc59894e.PNG"
+  },
+  {
+    "revision": "ec5ec6c0b91ace6b47a2224835d992aa",
+    "url": "/static/media/learn-quiz1.ec5ec6c0.PNG"
+  },
+  {
+    "revision": "3f2d756089210ae14864e5c7f907ea8a",
+    "url": "/static/media/learn-quiz2.3f2d7560.PNG"
+  },
+  {
+    "revision": "4a1fdcc778da2613e5d97456003cb5e5",
+    "url": "/static/media/learn-word.4a1fdcc7.PNG"
+  },
+  {
+    "revision": "8303dfc5e88e05db0573e001823bd2bb",
+    "url": "/static/media/marmibon-comment-section.8303dfc5.png"
+  },
+  {
+    "revision": "76d5e3d103e1867f20650dc189f8070b",
+    "url": "/static/media/marmibon-home.76d5e3d1.PNG"
+  },
+  {
+    "revision": "5026eed922a3ebdca2f9085086e1a14f",
+    "url": "/static/media/marmibon-profile.5026eed9.PNG"
+  },
+  {
+    "revision": "a9e781eb38581c1921e8c50bb562ccaa",
+    "url": "/static/media/marmibon-recipe.a9e781eb.PNG"
+  },
+  {
+    "revision": "ff6a3c08270d0e57e3a2a1b4ed5a46eb",
+    "url": "/static/media/marmibon-register.ff6a3c08.PNG"
+  },
+  {
+    "revision": "6178071d35fa2757eba7896c2793fe37",
+    "url": "/static/media/marmibon-search.6178071d.PNG"
+  },
+  {
+    "revision": "d515ef60dffa01a3627cced766eb8c6a",
+    "url": "/static/media/mongodb.d515ef60.png"
+  },
+  {
+    "revision": "e43276fcc3b37c0dfe06ec03418aab39",
+    "url": "/static/media/nodejs.e43276fc.png"
+  }
+]);
